c1e1700690c19942c65f50eadd80330b3c149e3e
Jiayi Lin <rlin1@brynmawr.edu>
2018-02-25 14:46:48 -0500

    before merge

--- tokenizer.c
+++ tokenizer.c
@@ -1,10 +1,12 @@
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 char* delimiter = ";&";
 
 typedef struct tokenizer{
   char *str;  //the string to parse
-  char* pos;  //position in string
+  char *pos;  //position in string
 }tokenizer;
 
 tokenizer* create_new_tokenizer(char* str){
@@ -14,11 +16,13 @@ tokenizer* create_new_tokenizer(char* str){
   return t;
 }
 
-char* get_next_token(tokenizer* t){
-  char* substring = "";
-  for (int i=0, char* cur = t->pos; i<strlen(t->str); i++, cur = t->pos+i){
+char* get_next_token(tokenizer* t, char substring[600]){
+  char* cur = t->pos;
+  for (int i=0; i<strlen(t->str); i++, cur = t->pos+i){
     for (int j=0; j <strlen(delimiter); j++){
-      if (*cur == *(delimiter+j)){
+      char ch = cur[0];
+      char d = delimiter[j];
+      if (ch == d){
         if(strcmp(substring, "")==0){
           return cur;
         }else{
@@ -26,14 +30,23 @@ char* get_next_token(tokenizer* t){
         }
       }
     }
-    substring = strcat(substring, cur);
+    strcat(substring, cur);
   }
   return NULL;
 }
 
 int main(int argc, char** argv){
-  char* buffer = NULL;
-  size_t num;
+  char* buffer = "This is my shell c;ode.\n";
+  size_t num = 10;
   size_t buffer_size;
-  num = getline(&buffer, &buffer_size; STDIN_FILENO);
+  // num = getline(&buffer, &buffer_size, 0);
+  // printf("%d\n", num);
+  if (num){
+    tokenizer* t = create_new_tokenizer(buffer);
+    char substring[600];
+    char* s = get_next_token(t, substring);
+    printf("%s\n",s);
+    // free(t);
+  }
+  return 0;
 }
