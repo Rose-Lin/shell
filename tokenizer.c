06ad50ce62ca01d3a71b66ba81e3acfd72fd10fc
Jiayi Lin <rlin1@brynmawr.edu>
2018-02-24 23:33:48 -0500

    before testing tokenizer

--- tokenizer.c
+++ tokenizer.c
@@ -1,3 +1,7 @@
+#include <string.h>
+
+char* delimiter = ";&";
+
 typedef struct tokenizer{
   char *str;  //the string to parse
   char* pos;  //position in string
@@ -8,4 +12,28 @@ tokenizer* create_new_tokenizer(char* str){
   t->str = str;
   t->pos = str;
   return t;
+}
+
+char* get_next_token(tokenizer* t){
+  char* substring = "";
+  for (int i=0, char* cur = t->pos; i<strlen(t->str); i++, cur = t->pos+i){
+    for (int j=0; j <strlen(delimiter); j++){
+      if (*cur == *(delimiter+j)){
+        if(strcmp(substring, "")==0){
+          return cur;
+        }else{
+          return substring;
+        }
+      }
+    }
+    substring = strcat(substring, cur);
+  }
+  return NULL;
+}
+
+int main(int argc, char** argv){
+  char* buffer = NULL;
+  size_t num;
+  size_t buffer_size;
+  num = getline(&buffer, &buffer_size; STDIN_FILENO);
 }
--- tokenizer.h
+++ tokenizer.h
@@ -1,3 +1,5 @@
 typedef struct tokenizer* tokenizer;
 
 tokenizer* create_new_tokenizer(char* str, char* pos);
+
+char* get_next_token(tokenizer* t);
